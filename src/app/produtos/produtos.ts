70dd07e0c11fb342799ac9fe9815485c73c790a6
sidneyrdm <sidneybritomal@gmail.com>
2024-03-26 21:21:14 -0300

    atualização da base mock dos itens

--- src/app/produtos/produtos.ts
+++ src/app/produtos/produtos.ts
@@ -12,19 +12,16 @@ export interface IProdutoCarrinho extends IProduto {
 }
 
 export const produtos: IProduto[] = [
-    { id: 1, descricao: "Mouse gamer", preco: 439.00, descricaoPreco: "À vista no PIX", imagem: "./assets/mouse-3.jpg", quantidadeEstoque: 10 },
-    { id: 2, descricao: "Monitor muito bom", preco: 1200.50, descricaoPreco: "À vista no PIX", imagem: "./assets/monitor-1.jpg", quantidadeEstoque: 10 },
-    { id: 3, descricao: "Teclado excelente", preco: 749.99, descricaoPreco: "À vista no PIX", imagem: "./assets/teclado-1.jpg", quantidadeEstoque: 10 },
-    { id: 4, descricao: "Fone para quem joga FPS", preco: 599.99, descricaoPreco: "À vista no PIX", imagem: "./assets/fone-de-ouvido-2.jpg", quantidadeEstoque: 10 },
-    { id: 5, descricao: "Fone de ouvido", preco: 299.99, descricaoPreco: "À vista no PIX", imagem: "./assets/fone-de-ouvido-1.jpg", quantidadeEstoque: 10 },
-    { id: 6, descricao: "Fone de ouvido bom", preco: 399.99, descricaoPreco: "À vista no PIX", imagem: "./assets/fone-de-ouvido-3.jpg", quantidadeEstoque: 10 },
-    { id: 7, descricao: "HD 1TB", preco: 499.99, descricaoPreco: "À vista no PIX", imagem: "./assets/hd.jpg", quantidadeEstoque: 10 },
-    { id: 8, descricao: "Combo de placa de vídeos", preco: 18449.99, descricaoPreco: "À vista no PIX", imagem: "./assets/placa-video.jpg", quantidadeEstoque: 10 },
-    { id: 9, descricao: "Processador Ryzen", preco: 1000, descricaoPreco: "À vista no PIX", imagem: "./assets/processador.jpg", quantidadeEstoque: 10 },
-    { id: 10, descricao: "Notebook bom", preco: 2500, descricaoPreco: "À vista no PIX", imagem: "./assets/laptop-1.jpg", quantidadeEstoque: 10 },
-    { id: 11, descricao: "Notebook excelente", preco: 4500, descricaoPreco: "À vista no PIX", imagem: "./assets/laptop-2.jpg", quantidadeEstoque: 10 },
-    { id: 12, descricao: "Mouse barato", preco: 20, descricaoPreco: "À vista no PIX", imagem: "./assets/mouse-1.png", quantidadeEstoque: 10 },
-    { id: 13, descricao: "Mouse ótimo", preco: 200, descricaoPreco: "À vista no PIX", imagem: "./assets/mouse-2.jpg", quantidadeEstoque: 10 },
-    { id: 14, descricao: "Mouse pequeno", preco: 50, descricaoPreco: "À vista no PIX", imagem: "./assets/mouse-4.jpg", quantidadeEstoque: 10 },
-    { id: 15, descricao: "Teclado bom", preco: 159.99, descricaoPreco: "À vista no PIX", imagem: "./assets/teclado-2.jpg", quantidadeEstoque: 10 },
+    { id: 1, descricao: "Mouse Gamer Bright Light, LED, 2400DPI, 6 Botões, Preto - 0447", preco: 38.90, descricaoPreco: "À vista no PIX", imagem: "./assets/mouse-gamer.jpeg", quantidadeEstoque: 10 },
+    { id: 2, descricao: "Monitor LG 27 IPS, Full HD, HDMI, VESA, Ajuste de Ângulo, FreeSync, Bordas Finas - 27MP400-B", preco: 798.99, descricaoPreco: "À vista no PIX", imagem: "./assets/monitor-27.jpeg", quantidadeEstoque: 10 },
+    { id: 3, descricao: "Teclado Mecânico Gamer Redragon Fizz, RAINBOW, Switch Blue, ABNT 2, Black, K617-R-B (PT-BLUE)", preco: 178.99, descricaoPreco: "À vista no PIX", imagem: "./assets/teclado-gamer.jpeg", quantidadeEstoque: 10 },
+    { id: 4, descricao: "Headset Gamer Bright, RGB, 7.1, USB, Microfone, LED, Preto - 0592", preco: 178.99, descricaoPreco: "À vista no PIX", imagem: "./assets/fone-gamer.jpeg", quantidadeEstoque: 10 },
+    { id: 5, descricao: "Notebook Samsung Book KP3BR, Intel Celeron 6305, 4GB, 256GB SSD, 15,6” Full HD, Windows 11, Cinza Chumbo - 550XDA-KP3", preco: 1828.99, descricaoPreco: "À vista no PIX", imagem: "./assets/notebook-samsung.jpeg", quantidadeEstoque: 10 },
+    { id: 6, descricao: "Placa de Vídeo Palit NVIDIA GeForce GT 1030, 2GB DDR4, 64 Bits, Low Profile - NEC103000646-1082F", preco: 478.99, descricaoPreco: "À vista no PIX", imagem: "./assets/placa-video.jpeg", quantidadeEstoque: 10 },
+    { id: 7, descricao: "Processador AMD Ryzen 7 5700G, AM4, Cache 20Mb, 3.8GHz (4.6GHz Max Turbo) - 100-100000263BOX", preco: 1278.99, descricaoPreco: "À vista no PIX", imagem: "./assets/ryzen.jpeg", quantidadeEstoque: 10 },
+    { id: 8, descricao: "Placa Mãe MSI Pro B550M-P GEN3, Chipset B550, AMD AM4, mATX, DDR4, DVI HDMI", preco: 818.99, descricaoPreco: "À vista no PIX", imagem: "./assets/placa-am4.jpeg", quantidadeEstoque: 10 },
+    { id: 9, descricao: "Gabinete Gamer Bluecase BG-024W, Lateral De Vidro Temperado, Sem Fonte, Branco - BG024WGCASE", preco: 149.99, descricaoPreco: "À vista no PIX", imagem: "./assets/gabinete-gamer.jpeg", quantidadeEstoque: 10 },
+    { id: 10, descricao: "SSD Redragon Blaze, 1TB, M.2 2280 NVMe PCIe 4.0, Leitura 7450MB/s, Gravação 6600MB/s - GD-707", preco: 548.99, descricaoPreco: "À vista no PIX", imagem: "./assets/ssd-redragon.jpeg", quantidadeEstoque: 10 },
+    { id: 11, descricao: "Memória Kingston Fury Beast EXPO, 16GB, 5200Mhz, DDR5, Preto - KF552C36BBE-16", preco: 518.99, descricaoPreco: "À vista no PIX", imagem: "./assets/memoria-ddr5.jpeg", quantidadeEstoque: 10 },
+    { id: 12, descricao: "Processador Intel Core i9-10900KF, LGA 1200, Cache 20Mb, 3.70GHz (5.3GHz Max Turbo) - BX8070110900KF", preco: 2449.99, descricaoPreco: "À vista no PIX", imagem: "./assets/core-i9.jpeg", quantidadeEstoque: 10 }
 ]
